eeab5779a7c5d1ad1d76f81ee6b07fb772e6c0dd
huykhaduy <huykhaduy@gmail.com>
2023-12-22 15:16:14 +0700

    tạo mấy cái migration

---
+++ database/migrations/2023_12_22_073757_create_rooms_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("type"); // Mục đích sử dụng
+            $table->enum("facility", ["c", "1", "2"]); // Cơ sở
+            $table->uuid("key"); // Tạo mã qr bằng cái này để tránh lộ mã phòng
+            $table->string("icon")->nullable();
+            $table->json("extra_data")->nullable(); // Extra data sẽ chứa range của location
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
---
+++ database/migrations/2023_12_22_074135_create_equipment_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('equipments', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("description")->nullable();
+            $table->string("icon")->nullable();
+            $table->foreign('type_id')->references('id')->on('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('equipment');
+    }
+};
